b0633e9a34ca5e16dc6a90a6c22acacca9b43c03
Erik Cunningham <ecunningham@twitter.com>
2017-04-27 11:47:48 -0400

    basic regl starting point.

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const regl = require("regl")();
+
+regl.clear({
+  color: [0, 0, 0, 1],
+  depth: 1
+});
+
+regl({
+  // In a draw call, we can pass the shader source code to regl
+  frag: `
+  precision mediump float;
+  uniform vec4 color;
+  void main () {
+    gl_FragColor = color;
+  }`,
+
+  vert: `
+  precision mediump float;
+  attribute vec2 position;
+  void main () {
+    gl_Position = vec4(position, 0, 1);
+  }`,
+
+  attributes: {
+    position: [
+      [-1, 0],
+      [0, -1],
+      [1, 1]
+    ]
+  },
+
+  uniforms: {
+    color: [1, 0, 0, 1]
+  },
+
+  count: 3
+})();
